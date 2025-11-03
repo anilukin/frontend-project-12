d6040009123f84c50d2cee0a52c5143b59338f90
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2025-11-03 16:17:22 +0000

    add: add rollbar: step 11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,22 @@ import { I18nextProvider } from 'react-i18next';
 import i18next from './i18n';
 import App from './App.jsx';
 import store from './Slices/index.js';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import rollbarConfig from './rollbar/rollbarConfig.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <I18nextProvider i18n={i18next} defaultNS={'translation'}>
-          <App />
-        </I18nextProvider>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      {/* <TestError /> */}
+      <StrictMode>
+        <Provider store={store}>
+          <BrowserRouter>
+            <I18nextProvider i18n={i18next} defaultNS={'translation'}>
+              <App />
+            </I18nextProvider>
+          </BrowserRouter>
+        </Provider>
+      </StrictMode>
+    </ErrorBoundary>
+  </RollbarProvider>
 );
---
+++ frontend/src/rollbar/rollbarConfig.js
@@ -0,0 +1,12 @@
+const env = import.meta.env;
+
+const rollbarConfig = {
+  accessToken: env.VITE_ROLLBAR_TOKEN,
+  payload: {
+    environment: 'production',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
+export default rollbarConfig;
