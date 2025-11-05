0531277671ac6c17df1968dea97fea72134e22f1
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2025-11-05 08:44:06 +0000

    fix: spaces: step 12

--- frontend/src/Components/Channel.jsx
+++ frontend/src/Components/Channel.jsx
@@ -28,31 +28,31 @@ const Channel = ({
             variant={channel.id === selectedChannelId ? 'secondary' : null}
             className="d-flex"
           >
-          <ChannelButton
-            channel={channel}
-            selectedChannelId={selectedChannelId}
-            handleClick={handleClick}
-          />
+            <ChannelButton
+              channel={channel}
+              selectedChannelId={selectedChannelId}
+              handleClick={handleClick}
+            />
 
-          <Dropdown.Toggle
-            variant={channel.id === selectedChannelId ? 'secondary' : null}
-            aria-expanded="false"
-            className="flex-grow-0"
-            split
-          >
-            <span className="visually-hidden">
-              {t('buttons.manageChannelButton')}
-            </span>
-          </Dropdown.Toggle>
+            <Dropdown.Toggle
+              variant={channel.id === selectedChannelId ? 'secondary' : null}
+              aria-expanded="false"
+              className="flex-grow-0"
+              split
+            >
+              <span className="visually-hidden">
+                {t('buttons.manageChannelButton')}
+              </span>
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item eventKey="1" onClick={() => handleRemove(channel)}>
-              {t('buttons.removeButton')}
-            </Dropdown.Item>
-            <Dropdown.Item eventKey="2" onClick={() => handleRename(channel)}>
-              {t('buttons.renameButton')}
-            </Dropdown.Item>
-          </Dropdown.Menu>
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="1" onClick={() => handleRemove(channel)}>
+                {t('buttons.removeButton')}
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="2" onClick={() => handleRename(channel)}>
+                {t('buttons.renameButton')}
+              </Dropdown.Item>
+            </Dropdown.Menu>
           </Dropdown>
         </li>
     )
